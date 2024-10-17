97486ddac3c1447289c7839ede9b290eb5bc08d0
otienojoseph <otienoj.cruz@gmail.com>
2024-10-17 13:26:44 +0300

    fix: add annotations

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """
     async coroutine that waits for a random delay and returns it.
 
