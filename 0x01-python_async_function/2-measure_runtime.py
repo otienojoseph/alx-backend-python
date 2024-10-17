6f115335cf1ab191a3c1446e26948e25711a9c95
otienojoseph <otienoj.cruz@gmail.com>
2024-10-17 18:02:28 +0300

    Add function measure_time

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Coroutine to measure runtime"""
+
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """
+    Function that measures the total execution time for wait_n
+
+    Args:
+        n (int): The number of times to run the Coroutine
+        max_delay (int): The maximum delay time for execution
+    Returns - The total_time / n
+    """
+    start_time = time.perf_counter()
+    await wait_n(n, max_delay)
+    total_time = time.perf_counter() - start_time
+
+    return (total_time / n)
