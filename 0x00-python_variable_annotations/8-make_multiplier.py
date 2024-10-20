f91dd6f66037023ef5076b44ab7f3135401e54ce
otienojoseph <otienoj.cruz@gmail.com>
2024-10-21 00:36:01 +0300

    add multiplier function

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""make_multiplier function"""
+
+from collections.abc import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Function that multiplies a float by a given multiplier
+
+    Args:
+        multiplier (float): Float multiplier
+
+    Returns: A funcion that multiplies a float by a multiplier
+    """
+    def multiply(x: float) -> float:
+        return x * multiplier
+
+    return multiply
