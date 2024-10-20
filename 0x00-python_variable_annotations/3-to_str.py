122c1199485f656f678c0dd2573d8db09819f1e7
otienojoseph <otienoj.cruz@gmail.com>
2024-10-20 22:37:43 +0300

    Add to_str function

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""To String Function"""
+
+
+def to_str(n: float) -> str:
+    """
+    Function that takes a float and returns its string representation
+
+    Args:
+        n (float): Float parameter
+
+    Returns: String representation of float
+    """
+    return str(n)
