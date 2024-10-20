5342d298322536efa7d04e3b306e43e94dededf4
otienojoseph <otienoj.cruz@gmail.com>
2024-10-20 17:26:04 +0300

    Add function the returns an asyncio object

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""3-tasks function"""
+
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay):
+    """
+    task_wait_random - function that takes an integer and returns an
+    `asyncio.Task`
+
+    Args:
+        max_delay (int): Max delay in seconds
+
+    Return: An `asyncio.Task` object
+    """
+    return asyncio.create_task(wait_random(max_delay))
